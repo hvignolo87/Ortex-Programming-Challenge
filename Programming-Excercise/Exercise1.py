54bc4389425d02fedeb3f262335c969b9bc4e0d6
Hernán Vignolo <vignolo.hernan@gmail.com>
2022-06-15 08:41:37 -0300

    Added path handling and fixed PEP8 format

--- Programming-Excercise/Exercise1.py
+++ Programming-Excercise/Exercise1.py
@@ -1,34 +1,43 @@
 # Import native CSV librarie
 import csv
 
+# For path handling
+import os
+
+# Save the current directory path
+dirname = os.path.dirname(__file__)
+
+# Define the CSV file path
+csv_path = os.path.join(dirname, '2017.csv')
+
 # Open the CSV file
-with open(file = '2017.csv', mode = 'r', encoding = 'utf8') as csv_file:
+with open(file=csv_path, mode='r', encoding='utf8') as csv_file:
     
     # Map each line in the CSV into a dictionary
-    transactions = csv.DictReader(csv_file, delimiter = ',')
+    transactions = csv.DictReader(csv_file, delimiter=',')
         
     # Create an empty dictionary to store the results
     db = {
-            'exchange_most_trans' : {},
-            'highest_value_EUR'   : {},
-            'trans_per_month'     : {}
-        }
+        'exchange_most_trans': {},
+        'highest_value_EUR': {},
+        'trans_per_month': {}
+    }
 
     # Map numbers to months in a dictionary
     month = {
-                '01':'Jan',
-                '02':'Feb',
-                '03':'Mar',
-                '04':'Apr',
-                '05':'May',
-                '06':'Jun',
-                '07':'Jul',
-                '08':'Aug',
-                '09':'Sep',
-                '10':'Oct',
-                '11':'Nov',
-                '12':'Dec'		
-            }
+        '01': 'Jan',
+        '02': 'Feb',
+        '03': 'Mar',
+        '04': 'Apr',
+        '05': 'May',
+        '06': 'Jun',
+        '07': 'Jul',
+        '08': 'Aug',
+        '09': 'Sep',
+        '10': 'Oct',
+        '11': 'Nov',
+        '12': 'Dec'		
+    }
 
     # Iterate the data
     for transaction in transactions:
@@ -56,16 +65,16 @@ with open(file = '2017.csv', mode = 'r', encoding = 'utf8') as csv_file:
 # Question and answer 1
 print('Question 1: What Exchange has had the most transactions in the file?')
 most_trans = max(db['exchange_most_trans'], 
-                 key = db['exchange_most_trans'].get)
-print('Answer 1: {}' . format(most_trans))
+                 key=db['exchange_most_trans'].get)
+print('Answer 1: {}'.format(most_trans))
 print('\n')
 
 # Question and answer 2
 print('Question 2: In August 2017, which companyName ' + \
       'had the highest combined valueEUR?')
 highest_key = max(db['highest_value_EUR'], 
-                  key = db['highest_value_EUR'].get)
-print('Answer 2: {}' . format(highest_key))
+                  key=db['highest_value_EUR'].get)
+print('Answer 2: {}'.format(highest_key))
 print('\n')
 
 # Question and answer 3
@@ -78,4 +87,4 @@ print('Answer 3: ')
 for key in db['trans_per_month']:
     db['trans_per_month'][key] /= total
     db['trans_per_month'][key] *= 100
-    print(key + ', {}%' . format(round(db['trans_per_month'][key])))
+    print(key + ', {}%'.format(round(db['trans_per_month'][key], 2)))
